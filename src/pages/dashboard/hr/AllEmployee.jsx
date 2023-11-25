b3d06bd3436fdad0b9341cce62528ba4f56cd6e4
aubonee <chytahsin.2210@gmail.com>
2023-11-25 22:30:43 +0600

    modal on pay button added

--- src/pages/dashboard/hr/AllEmployee.jsx
+++ src/pages/dashboard/hr/AllEmployee.jsx
@@ -5,10 +5,20 @@ import { Link } from 'react-router-dom';
 import { FaCheck  } from 'react-icons/fa';
 import { ImCross } from "react-icons/im";
 import Swal from 'sweetalert2';
+import  { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import MonthPicker from 'react-month-picker';
 
 
 const AllEmployee = () => {
     const axiosSecure =useAxiosSecure();
+    
+    const [selectedDate, setSelectedDate] = useState(null);
+      
+    const handleDateChange = (date) => {
+      setSelectedDate(date);
+    };
   
     const { data:users=[] , refetch}=useQuery({
         //refetch
@@ -86,6 +96,8 @@ const AllEmployee = () => {
         console.error('Error updating user verification:', error);
       });
 
+      
+
 }
     return (
         <div>
@@ -123,22 +135,65 @@ const AllEmployee = () => {
             <td> {employee.email}</td>
             {/* <td><button className='btn text-white bg-purple-500'>Make Verified</button></td> */}
             <td>
-              {employee.isVerified == 'verified'? <button onClick={()=>handleMakeUnVerified(employee)}  className="btn  p-3 py-1 my-2 text-red-500 text-lg "> <ImCross /> </button> : <button onClick={()=>handleMakeVerified(employee)}  className="btn  p-3 py-1 my-2  text-lg text-green-700"><FaCheck/></button>}
+              {employee.isVerified == 'verified'? <button onClick={()=>handleMakeUnVerified(employee)}  className="btn  p-3 py-1 my-2 text-green-500 text-lg "><FaCheck/> </button> : <button onClick={()=>handleMakeVerified(employee)}  className="btn  p-3 py-1 my-2  text-lg text-red-700"><ImCross /> </button>}
                
+              <div>
+  
+
+  <dialog id="my_modal_3" className="modal">
+    <div className="modal-box min-h-[300px]">
+      <form method="dialog">
+        {/* if there is a button in form, it will close the modal */}
+        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+      </form >
+      <h3 className="font-bold text-lg">Salary:{employee.salary}</h3>
+<form className='flex flex-col items-center justify-center'>
+     
+     <div className="flex gap-5">
+     <div className='form-control'>
+     <label className="label">
+    <span className="label-text  font-bold">Choose the Month:</span>
+  </label>
+      <DatePicker className='input input-bordered w-full max-w-xs'
+        selected={selectedDate}
+        onChange={handleDateChange}
+        showMonthYearPicker
+        dateFormat="MM"
+        
+      />
+     </div>
+     <div className='form-control'>
+     <label className="label">
+    <span className="label-text  font-bold">Choose the Year:</span>
+  </label>
+      <DatePicker className='input input-bordered w-full max-w-xs'
+        selected={selectedDate}
+        onChange={handleDateChange}
+        showMonthYearPicker
+        dateFormat="yyyy"
+        
+      />
+     </div>
+     </div>
+     <div> <button className='btn btn-primary my-5' type="submit">Submit</button></div>
+     </form>
+
+    
+    </div>
+  </dialog>
+    </div>
               </td>
             <td> {employee.bank_account_no}</td>
             <td>{employee.salary}</td>
             <td>{employee.role}</td>
-            <td> <button className='btn text-white bg-purple-500'>Pay</button></td>
+            <td> <button className='btn text-white bg-purple-500'onClick={()=>document.getElementById('my_modal_3').showModal()} disabled={employee.isVerified !== 'verified'}>Pay</button></td>
             <td> <Link to={`/dashboard/employeeDetail/${employee._id}`}  className='btn text-white bg-purple-500'>Details</Link></td>
         </tr>
         )
      } 
     </tbody>
-    {/* foot */}
-   
-    
   </table>
+  
 </div>
         </div>
     );
